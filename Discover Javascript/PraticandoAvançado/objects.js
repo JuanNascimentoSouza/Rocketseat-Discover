77e1e01e5c050d01d35b360ae36813d0fe5b2e4b
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-06-06 20:42:48 -0300

    Scope javascript

---
+++ Discover Javascript/PraticandoAvançado/objects.js
@@ -0,0 +1,12 @@
+// Object
+
+const person = {
+    name: "John",
+    agr: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person.age)
+// ou
+console.log(`${person.name} tem ${person.age} anos`)
