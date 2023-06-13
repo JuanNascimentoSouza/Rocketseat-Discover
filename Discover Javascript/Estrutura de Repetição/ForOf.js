8f7a9c28fb292bd51e43ebbc8d641393487aaa8b
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-06-13 20:30:24 -0300

    for in, for of

---
+++ Discover Javascript/Estrutura de Repetição/ForOf.js
@@ -0,0 +1,9 @@
+
+// for...of
+
+let name = 'Eu'
+let name2 = ['João ', 'Pedro', 'Thiago']
+
+for (let name of name2) {
+    console.log(name)
+}
---
+++ Discover Javascript/Exercicios JS/NotasEscolares.js
@@ -0,0 +1,15 @@
+
+let x = 91
+
+if (x >= 90) {
+    console.log('Nota A')
+} else if (x >= 80) {
+    console.log('Nota B')
+} else if (x >= 70) {
+    console.log('Nota C')
+} else if (x >= 60) {
+    console.log('Nota D')
+} else if (x <= 59) {
+    console.log('Nota F')
+}
+console.log(x)
